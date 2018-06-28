03786422cee69307696affafbf21c9a92238f2e8
bryanfong.nyc@gmail.com <bryanfong.nyc@gmail.com>
2018-06-28 04:27:11 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,17 @@
 # code your #position_taken? method here!
 
 def position_taken?(board,index)
+<<<<<<< HEAD
   if board[index] == "" || board[index] == " " || board[index] == nil
+=======
+  if board[index] == " " || "" || nil
+>>>>>>> 16402430480d10ff2e190ba93bece60fc7944f35
     return false 
   else
     return true 
   end 
+<<<<<<< HEAD
 end 
+=======
+end 
+>>>>>>> 16402430480d10ff2e190ba93bece60fc7944f35
